b9999b8e01b6f90c7cbad44cd57b3c273de1a3fe
Chirag Mittal <chirag.workid@gmail.com>
2022-06-13 15:53:46 +0530

    try to fix stuck wrror

--- frontend/src/components/Authentication/Login.js
+++ frontend/src/components/Authentication/Login.js
@@ -49,7 +49,7 @@ const Login = () => {
             });
             localStorage.setItem('userInfo',JSON.stringify(data));
             setLoading(false);
-            history.push('/chats');
+            history.push('https://scholar-chat-orcid.herokuapp.com/chats');
         } catch (error) {
             toast({
                 title:'Error Occured!',
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,11 +7,13 @@ import ChatProvider from './Context/ChatProvider';
 
 ReactDOM.render(
   <BrowserRouter>
+  {/* <React.StrictMode> */}
     <ChatProvider>
       <ChakraProvider>
         <App />
       </ChakraProvider>
     </ChatProvider>
+    {/* </React.StrictMode> */}
   </BrowserRouter>,
   document.getElementById('root')
 );
