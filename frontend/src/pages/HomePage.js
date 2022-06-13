8250a075e6e6857adb00e8430f6b25446c81fdef
kuchiPie <shreyasbulbule007@gmail.com>
2022-06-13 18:35:18 +0530

    fixed the refresh error

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,46 +1,77 @@
-import React ,{useEffect} from 'react'
-import { Container ,Box, Text, TabList, Tab, TabPanels,TabPanel,Tabs} from '@chakra-ui/react'
-import Login from '../components/Authentication/Login'
-import Signup from '../components/Authentication/Signup'
-import { useHistory } from 'react-router-dom'
+import React, { useEffect } from "react";
+import {
+  Container,
+  Box,
+  Text,
+  TabList,
+  Tab,
+  TabPanels,
+  TabPanel,
+  Tabs,
+} from "@chakra-ui/react";
+import Login from "../components/Authentication/Login";
+import Signup from "../components/Authentication/Signup";
+import { useHistory } from "react-router-dom";
 
 const HomePage = () => {
   const history = useHistory();
-  useEffect(()=>{
-      const userInfo = JSON.parse(localStorage.getItem("userInfo"));
-      if(userInfo){
-          history.push('/chats')
-      }
-  },[history]);
+  useEffect(() => {
+    const userInfo = JSON.parse(localStorage.getItem("userInfo"));
+    if (userInfo) {
+      history.push("/chats");
+    }
+  }, [history]);
+  function checkIfLoggedIn() {
+    const history = useHistory();
+    const userInfo = JSON.parse(localStorage.getItem("userInfo"));
+    if (userInfo) {
+      history.push("/chats");
+    }
+  }
 
   return (
-    <Container maxW='xl' centerContent>
-
-      <Box d='flex' justifyContent='center' p={3} bg={'white'} w='100%' m='40px 0 15px 0' borderRadius='1g' borderWidth='1px'>
-        <Text fontSize='4xl' fontFamily='Work sans' color='black'>Scholar Chat</Text>
+    <Container maxW="xl" centerContent onLoad={checkIfLoggedIn}>
+      <Box
+        d="flex"
+        justifyContent="center"
+        p={3}
+        bg={"white"}
+        w="100%"
+        m="40px 0 15px 0"
+        borderRadius="1g"
+        borderWidth="1px"
+      >
+        <Text fontSize="4xl" fontFamily="Work sans" color="black">
+          Scholar Chat
+        </Text>
       </Box>
 
-      <Box bg='white' w='100%' p={4} borderRadius='1g' color='black' borderWidth='1px'>
-
-        <Tabs variant='soft-rounded'>
-          <TabList mb={'1em'}>
-            <Tab width={'50%'}>Login</Tab>
-            <Tab width={'50%'}>Register</Tab>
+      <Box
+        bg="white"
+        w="100%"
+        p={4}
+        borderRadius="1g"
+        color="black"
+        borderWidth="1px"
+      >
+        <Tabs variant="soft-rounded">
+          <TabList mb={"1em"}>
+            <Tab width={"50%"}>Login</Tab>
+            <Tab width={"50%"}>Register</Tab>
           </TabList>
 
           <TabPanels>
             <TabPanel>
-              <Login/>
+              <Login />
             </TabPanel>
             <TabPanel>
-              <Signup/>
+              <Signup />
             </TabPanel>
           </TabPanels>
         </Tabs>
-
       </Box>
     </Container>
-  )
-}
+  );
+};
 
 export default HomePage;
