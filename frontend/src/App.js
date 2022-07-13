f22f324d40b165bf539402a6d80d85b6daeaf573
kuchiPie <shreyasbulbule007@gmail.com>
2022-07-13 14:33:30 +0530

    fixed extra / and added a redirect

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,15 @@
-import {Route,Switch} from 'react-router-dom';
-import ChatPage from './pages/ChatPage';
-import HomePage from './pages/HomePage';
-import './App.css'
-import VerifyPage from './pages/VerifyPage';
+import { Route, Switch } from "react-router-dom";
+import ChatPage from "./pages/ChatPage";
+import HomePage from "./pages/HomePage";
+import "./App.css";
+import VerifyPage from "./pages/VerifyPage";
 
 function App() {
   return (
     <div className="App">
-        <Route path='/' component={HomePage}></Route>
-        <Route path='/chats' component={ChatPage}></Route>
-        <Route path='/verify/:id' component={VerifyPage}></Route>
+      <Route path="/verify" component={VerifyPage} exact></Route>
+      <Route path="/" component={HomePage} exact></Route>
+      <Route path="/chats" component={ChatPage} exact></Route>
     </div>
   );
 }
--- frontend/src/pages/VerifyPage.js
+++ frontend/src/pages/VerifyPage.js
@@ -1,15 +1,29 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
+import React from "react";
+import { Link, useParams } from "react-router-dom";
+import { Text, Container, Box } from "@chakra-ui/react";
 
 const VerifyPage = () => {
-    const {id,token} = useParams();
   return (
-    <>
-        {id}
-        {token}
-        {console.log(id)}
-    </>
-  )
-}
+    <Container maxW="xl" centerContent>
+      <Box
+        d={"flex"}
+        justifyContent={"space-between"}
+        w={"100%"}
+        h={"100%"}
+        p={"50px"}
+      >
+        <Text>
+          Your email is successfully registered! Please click{" "}
+          {/* <Link to="/" style={{ color: "#0000FF" }}>
+            {" "}
+            here{" "}
+          </Link>{" "} */}
+          {console.log("Ha bhai mai aya fir mujhe nikal diya gaya")}
+          and login again to use the application.
+        </Text>
+      </Box>
+    </Container>
+  );
+};
 
-export default VerifyPage
+export default VerifyPage;
